81da8bd16c44b3956eab3577ef1d41fd3d2b99c5
SriramBharadwaj1 <sriram9217@gmail.com>
2023-05-01 09:55:48 +0530

    pwa functionality added

---
+++ sw.js
@@ -0,0 +1,63 @@
+
+
+self.addEventListener('install',event =>{
+    console.log('Install event!');
+
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(cache => {
+            return cache.addAll(resourcesToPrecache);
+        })
+    )
+});
+
+self.addEventListener('activate',() => {
+    console.log('Activate event');
+});
+
+self.addEventListener('fetch',event =>{
+    event.respondWith(caches.match(event.request)
+    .then(cachedResponse => {
+
+        return cachedResponse || fetch(event.request);
+    })
+    );
+});
+
+const cacheName = 'cache-v1';
+
+const resourcesToPrecache = [
+    '/',
+    'style.css',
+    'index.js',
+    'emi_logo.png'
+];
+
+let deferredPrompt;
+
+if (typeof window !='undefined')
+{
+    window.addEventListener('beforeinstallprompt',(e) => {
+    e.preventDefault();
+    deferredPrompt=e;
+
+    btnAdd.style.display ='block';
+    btn.addEventListener('click',(e) => {
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if(choiceResult.outcome =='accepted'){
+                console.log('User accepted the prompt');
+            }
+    
+            deferredPrompt = null;
+        })
+    });
+});
+
+window.addEventListener('appInstalled',(evt) => {
+    AudioParamMap.logEvent('a2hs','installed');
+})
+}
+
+
+
